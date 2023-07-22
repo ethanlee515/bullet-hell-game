b59f4753fa4ade6ddf73dcd80736bc86a6e84f50
ethanlee515 <ethanlee515@gmail.com>
2023-07-22 16:17:47 +0800

    Add UE template

---
+++ Source/BulletHell/BulletHell.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BulletHell.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BulletHell, "BulletHell" );
+
+DEFINE_LOG_CATEGORY(LogBulletHell)
+ 
---
+++ Source/BulletHell/BulletHellGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BulletHellGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABulletHellGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABulletHellGameMode();
+};
+
+
+
